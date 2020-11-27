6e42af81c0c72c7d3367df3cf80052fd3cac687d
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-11-27 08:11:00 +0000

    Lab-4-Zadanie-2

--- lab4.cpp
+++ lab4.cpp
@@ -9,6 +9,8 @@ public:
     T getPierwszyObiekt() const { return pierwszy_obiekt; }
     T getDrugiObiekt() const { return drugi_obiekt; }
 
+    T suma() const { return pierwszy_obiekt + drugi_obiekt; }
+
 private:
     T pierwszy_obiekt, drugi_obiekt;
 };
@@ -20,9 +22,14 @@ int main()
 
     Para< double > paraDouble(1.1, 2.2);
     std::cout << paraDouble.getPierwszyObiekt() << std::endl;
+
+    std::cout << paraInt.suma() << std::endl;
+    std::cout << paraDouble.suma() << std::endl;
 }
 
 /* Po uruchomieniu otrzymano:
   2
   1.1
+  3
+  3.3
 */
