dc71f8ef9cc5519fc338b67e3ad5950bf6c11dad
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-11-27 08:00:41 +0000

    Lab-4-Zadanie-1

--- lab4.cpp
+++ lab4.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+template < typename T >
+class Para
+{
+public:
+    Para(T pobiekt, T dobiekt) : pierwszy_obiekt{pobiekt}, drugi_obiekt{dobiekt} {}
+
+    T getPierwszyObiekt() const { return pierwszy_obiekt; }
+    T getDrugiObiekt() const { return drugi_obiekt; }
+
+private:
+    T pierwszy_obiekt, drugi_obiekt;
+};
+
 int main()
 {
-    puts("Hello, World!");
+    Para< int > paraInt(1, 2);
+    std::cout << paraInt.getDrugiObiekt() << std::endl;
+
+    Para< double > paraDouble(1.1, 2.2);
+    std::cout << paraDouble.getPierwszyObiekt() << std::endl;
 }
+
+/* Po uruchomieniu otrzymano:
+  2
+  1.1
+*/
